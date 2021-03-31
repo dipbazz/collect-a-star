9f199659c3b7cc6242460c8bc6c3192695e4dcaa
Dipesh Bajgain <dipbazz@gmail.com>
2021-03-31 21:49:05 +0545

    Add game logic that can move player and interact with world and collect star

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import Phaser, {Game} from 'phaser'
 
-const canvas = document.getElementById('game-canvas');
 const config = {
-  type: Phaser.WEB_GL,
-  width: 400,
-  height: 300,
-  canvas,
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 400 },
-      debug: true
+      gravity: { y: 300 },
+      debug: false
     }
   },
   scene: {
@@ -22,14 +20,141 @@ const config = {
 
 const game = new Game(config);
 
-const preload = () => {
+function preload() {
+  this.load.image('sky', 'assets/sky.png');
+  this.load.image('ground', 'assets/platform.png');
+  this.load.image('star', 'assets/star.png');
+  this.load.image('bomb', 'assets/bomb.png');
 
+  this.load.spritesheet('dude',
+    'assets/dude.png',
+    {
+      frameWidth: 32,
+      frameHeight: 48
+    }
+  )
 }
 
-const create = () => {
+let platforms;
+let player;
+let cursors;
+let stars;
+let score = 0;
+let scoreText;
+let bombs;
+let gameOver;
 
+function create() {
+  this.add.image(400, 300, 'sky');
+
+  platforms = this.physics.add.staticGroup();
+
+  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+  platforms.create(600, 400, 'ground');
+  platforms.create(50, 250, 'ground');
+  platforms.create(750, 220, 'ground');
+
+  player = this.physics.add.sprite(100, 450, 'dude');
+
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', {
+      start: 0,
+      end: 3
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: 'turn',
+    frames: [ {key: 'dude', frame: 4 }],
+    frameRate: 20
+  });
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', {
+      start: 5,
+      end: 8
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.physics.add.collider(player, platforms);
+
+  cursors = this.input.keyboard.createCursorKeys();
+
+  stars = this.physics.add.group({
+    key: 'star',
+    repeat: 11,
+    setXY: { x: 12, y: 0, stepX: 70 }
+  });
+
+  stars.children.iterate(function (child) {
+    child.setBounceY(Phaser.Math.FloatBetween(0.1, 0.4));
+  });
+
+  this.physics.add.collider(stars, platforms);
+
+  this.physics.add.overlap(player, stars, collectStar, null, this);
+
+  scoreText = this.add.text(16, 16, 'score: 0', {
+    fontSize: '32px',
+    fill: '#000'
+  });
+
+  bombs = this.physics.add.group();
+  this.physics.add.collider(bombs, platforms);
+  this.physics.add.collider(player, bombs, hitBomb, null, this);
 }
 
-const update = () => {
+function update() {
+  if(cursors.left.isDown) {
+    player.setVelocityX(-160);
+    player.anims.play('left', true);
+  } else if(cursors.right.isDown) {
+    player.setVelocityX(160);
+    player.anims.play('right', true);
+  } else {
+    player.setVelocityX(0);
+    player.anims.play('turn');
+  }
 
+  if(cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-330);
+  }
+}
+
+
+function collectStar(player, star) {
+  star.disableBody(true, true);
+
+  score += 10;
+  scoreText.setText('Score: ' + score);
+
+  if (stars.countActive(true) === 0) {
+    stars.children.iterate(function (child) {
+      child.enableBody(true, child.x, 0, true, true);
+    });
+
+    let x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+    let bomb = bombs.create(x, 16, 'bomb');
+    bomb.setBounce(1);
+    bomb.setCollideWorldBounds(true);
+    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+  }
+}
+
+function hitBomb(player, bomb) {
+  this.physics.pause();
+
+  player.setTint(0xff0000);
+  player.anims.play('turn');
+  gameOver = true;
 }
