c07b95b883c95268a2e380983cd3dc321ffb8e30
Dipesh Bajgain <dipbazz@gmail.com>
2021-04-02 13:30:15 +0545

    Add config to center the canvas

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  scale: {
+    mode: Phaser.Scale.NONE,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
   physics: {
     default: 'arcade',
     arcade: {
